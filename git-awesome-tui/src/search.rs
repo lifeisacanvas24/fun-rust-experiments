a9664ad5850e59bd3fadedeed2bfa066eb915a6a
lifeisacanvas24 <agriyain2k@gmail.com>
2024-11-09 21:56:23 +0530

    feat: Add fuzzy search for categories, subcategories and links [git-awesome-tui/src/search.rs]

--- git-awesome-tui/src/search.rs
+++ git-awesome-tui/src/search.rs
@@ -1,16 +1,52 @@
-use crate::models::Data;
-use crate::search;
+use crate::data::{Category, Link, Subcategory};
+use fuzzy_search::search;
 
-pub fn fuzzy_search(data: &Data, _query: &str) -> Vec<String> {
+pub fn fuzzy_search(query: &str, categories: &[Category]) -> Vec<usize> {
     let mut results = Vec::new();
-    for category in &data.categories {
-        for subcategory in &category.subcategories {
-            let matches = search::fuzzy_search(data, &subcategory.title);
-            if matches.is_empty() {
-                continue;
-            }
-            results.push(subcategory.title.clone());
-        }
-    }
+
+    // Search through category titles
+    results.extend(
+        categories
+            .iter()
+            .enumerate()
+            .filter_map(|(index, category)| {
+                if search(&category.title, query).is_some() {
+                    Some(index)
+                } else {
+                    None
+                }
+            }),
+    );
+
+    // Search through subcategory titles
+    results.extend(categories.iter().enumerate().flat_map(|(index, category)| {
+        category
+            .subcategories
+            .iter()
+            .enumerate()
+            .filter_map(|(sub_index, subcategory)| {
+                if search(&subcategory.title, query).is_some() {
+                    Some((index, sub_index)) // Returning category and subcategory index
+                } else {
+                    None
+                }
+            })
+    }));
+
+    // Search through link titles
+    results.extend(categories.iter().enumerate().flat_map(|(index, category)| {
+        category
+            .direct_links
+            .iter()
+            .enumerate()
+            .filter_map(|(link_index, link)| {
+                if search(&link.title, query).is_some() {
+                    Some((index, link_index)) // Returning category and link index
+                } else {
+                    None
+                }
+            })
+    }));
+
     results
 }
